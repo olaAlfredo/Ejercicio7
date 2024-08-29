564797cc3b851f76c4e6c38132b943e122a7c2cf
olaAlfredo <128110087+olaAlfredo@users.noreply.github.com>
2024-08-28 21:26:30 -0700

    Add files via upload

---
+++ Ejercicio7.js
@@ -0,0 +1,5 @@
+// 7. Concatenación de arreglos: Crea dos arreglos de strings y luego concatenarlos en un solo arreglo.
+const arreglo4 = ["Hola"];
+const arreglo5 = ["Mundo"];
+const concatenar = arreglo4.concat(arreglo5);
+console.log("7. Arreglo completo: "+concatenar);
